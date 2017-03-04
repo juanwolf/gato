1ecae38d7b73135a63a1dc3e3b9003b8151f7cda
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2017-03-04 23:00:29 +0000

    Cleaning consumer file.

--- gato/chat/consumers.py
+++ gato/chat/consumers.py
@@ -1,27 +1,4 @@
-from channels import Group
 from channels.generic import websockets
-
-import json
-
-
-# def ws_add(message):
-#     message.reply_channel.send({"accept": True})
-#     Group("chat").add(message.reply_channel)
-#
-#
-# def ws_message(message):
-#     print(message.content)
-#     mes = json.loads(message.content['text'])
-#     Group("chat").send({
-#         "text": "[%s] %s" % (
-#             mes['username'],
-#             mes['message']
-#         )
-#     })
-#
-#
-# def ws_disconnect(message):
-#     Group("chat").discard(message.reply_channel)
 
 
 class ChannelConsumer(websockets.JsonWebsocketConsumer):
