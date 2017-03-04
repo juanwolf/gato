5787108d5a80c59169a9fcf925be57e42cdc90d1
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2017-03-04 22:58:51 +0000

    Replacing consumers by a Generic Json Consumer.

--- gato/chat/consumers.py
+++ gato/chat/consumers.py
@@ -1,22 +1,35 @@
 from channels import Group
+from channels.generic import websockets
+
 import json
 
 
-def ws_add(message):
-    message.reply_channel.send({"accept": True})
-    Group("chat").add(message.reply_channel)
+# def ws_add(message):
+#     message.reply_channel.send({"accept": True})
+#     Group("chat").add(message.reply_channel)
+#
+#
+# def ws_message(message):
+#     print(message.content)
+#     mes = json.loads(message.content['text'])
+#     Group("chat").send({
+#         "text": "[%s] %s" % (
+#             mes['username'],
+#             mes['message']
+#         )
+#     })
+#
+#
+# def ws_disconnect(message):
+#     Group("chat").discard(message.reply_channel)
 
 
-def ws_message(message):
-    print(message.content)
-    mes = json.loads(message.content['text'])
-    Group("chat").send({
-        "text": "[%s] %s" % (
-            mes['username'],
-            mes['message']
-        )
-    })
+class ChannelConsumer(websockets.JsonWebsocketConsumer):
 
+    strict_ordering = False
 
-def ws_disconnect(message):
-    Group("chat").discard(message.reply_channel)
+    def connection_groups(self, **kwargs):
+        return ["chat"]
+
+    def receive(self, message, **kwargs):
+        self.send(message)
--- gato/chat/routing.py
+++ gato/chat/routing.py
@@ -1,10 +1,6 @@
-from channels.routing import route
-
 from chat import consumers
 
 
 channel_routing = [
-    route("websocket.receive", consumers.ws_message),
-    route("websocket.connect", consumers.ws_add),
-    route("websocket.disconnect", consumers.ws_disconnect)
+    consumers.ChannelConsumer.as_route(path=r'^/chat/'),
 ]
